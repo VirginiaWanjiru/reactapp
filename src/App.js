0365665eb33283fe3561540159c6ed243d19fe53
Virginia Wanjiru <virginiashee55@gmail.com>
2024-01-29 18:17:07 +0300

    refactor: redo code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './styles.css';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
